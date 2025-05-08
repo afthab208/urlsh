10e17d2b8ee2ec229d4cdfc457f0818918421832
Afthab <75113201+afthab208@users.noreply.github.com>
2025-05-08 18:51:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port =3000;
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+app.get('/', (req,res)=> {
+    res.send("Url.sh")
+})
+
+const urls ={};
+app.use(express.static('.'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/sh',(req,res)=> {
+    const url = req.body.url;
+    const id = nanoid(5);
+    urls[id]=url;
+
+    res.send(`<p>Short URL: <a href="/${id}" target="_blank">http://localhost:${port}/${id}</a></p>`)
+})
+app.get('/:id', (req,res)=>{
+    const url= urls[req.params.id];
+    if(url){
+        res.redirect(url);
+    }else {
+        res.send(`<h3>Request url not found :( </h3>`)
+    }
+})
+app.listen(port, ()=> {
+    console.log(`app listening at port  ${port}`)
+}) 
